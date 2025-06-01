01337e4fb93f3e4c8ebb95941d8ac174cca29810
Saransh <saranshaswal27@gmail.com>
2025-06-01 15:20:16 +0530

    third commit

--- E-commerceB/app.js
+++ E-commerceB/app.js
@@ -1,12 +1,17 @@
 let express =require("express");
 let app = express();
 let cors = require('cors');
+const path = require("path");
+const _dirname= path.dirname("");
+console.log(_dirname);
+const buildpath = path.join(_dirname,"../E-commerceF/dist");
 let itemRoutes =require("./routes/itemsRoutes")
 let UserRoutes = require("./routes/userRoutes")
 let paymentRoutes = require("./routes/paymentRoutes")
 // middleware 
 app.use(express.json())
 app.use(cors());
+app.use(express.static(buildpath));
 // routes 
 app.use("/app/v1/user",UserRoutes);
 app.use("/app/v1/items",itemRoutes);
