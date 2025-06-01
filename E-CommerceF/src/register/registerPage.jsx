2b583a5e8583fe367370d22dcc6b177904ccef32
Saransh <saranshaswal27@gmail.com>
2025-06-01 17:49:45 +0530

    eighth commit

--- E-CommerceF/src/register/registerPage.jsx
+++ E-CommerceF/src/register/registerPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import "../register/registerpage.css"
+import "../register/registerPage.css"
 import axios from "axios"
 import {Link, useNavigate} from "react-router-dom"
 export default function Register() {
